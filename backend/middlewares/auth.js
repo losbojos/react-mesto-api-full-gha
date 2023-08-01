b40b102f3a0e73217e32e550ce6c8c10d7dc5837
losbojos <losbojos@yandex.ru>
2023-08-01 23:56:44 +0700

    fix: else после next не хватает

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -20,29 +20,29 @@ const checkJwtToken = (req, res, next) => {
 
   if (!authorization) {
     next(new AuthError('Требуется авторизация'));
-  }
-
-  let token = null;
-  if (authorization.startsWith('Bearer ')) {
-    token = authorization.replace('Bearer ', '');
-  }
-
-  if (!token) {
-    next(new AuthError('Токен не передан или передан не в том формате'));
   } else {
-    jwt.verify(token, getSecretKey(), (err, payload) => {
-      if (err) {
-        next(new AuthError(err));
-      }
+    let token = null;
+    if (authorization.startsWith('Bearer ')) {
+      token = authorization.replace('Bearer ', '');
+    }
 
-      User.findById(payload._id)
-        .orFail(new AuthError('Пользователь не найден'))
-        .then(() => {
-          req.user = { _id: payload._id }; // записываем пейлоуд в объект запроса
-          next(); // пропускаем запрос дальше
-        })
-        .catch(next);
-    });
+    if (!token) {
+      next(new AuthError('Токен не передан или передан не в том формате'));
+    } else {
+      jwt.verify(token, getSecretKey(), (err, payload) => {
+        if (err) {
+          next(new AuthError(err));
+        } else {
+          User.findById(payload._id)
+            .orFail(new AuthError('Пользователь не найден'))
+            .then(() => {
+              req.user = { _id: payload._id }; // записываем пейлоуд в объект запроса
+              next(); // пропускаем запрос дальше
+            })
+            .catch(next);
+        }
+      });
+    }
   }
 };
 
